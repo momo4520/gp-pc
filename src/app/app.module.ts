bf76ad5ce65ded70f7cb9a6d4c0553fc1b4153f8
momo <xchxy222@163.com>
2017-07-10 11:26:51 +0800

    股票管理平台创建

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+import { StockMangerComponent } from './stock/stock-manger/stock-manger.component';
+import { StarsComponent } from './stars/stars.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    SidebarComponent,
+    FooterComponent,
+    ContentComponent,
+    StockMangerComponent,
+    StarsComponent
   ],
   imports: [
     BrowserModule,
